e734f1a1c8ad550c78cb6be9f040d05b24f0fde6
William Summers <william.summers@gmail.com>
2025-04-28 17:31:56 -0500

    delete dist from commit

--- dist/dts/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { XJX } from './XJX';
-export { XJX };
-export { Configuration } from './core/types/config-types';
-export { DEFAULT_CONFIG } from './core/config/config';
-export { XJXError } from './core/types/error-types';
-export { ValueTransformer } from './core/transformers/ValueTransformer';
-export default XJX;

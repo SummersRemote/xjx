0c35c1ac96e119866fe9f0bcb1244f75db35d9aa
William Summers <william.summers@gmail.com>
2025-05-12 17:19:21 -0500

    removed getPath and getSchema and config management methods

--- src/extensions/index.ts
+++ src/extensions/index.ts
@@ -12,14 +12,13 @@
 import './terminal/to-xml';
 import './terminal/to-json';
 import './terminal/to-json-string';
-import './terminal/get-json-schema';
-import './terminal/get-path';
+// import './terminal/get-json-schema';
+// import './terminal/get-path';
 
 // Non-terminal extensions (methods that return the builder for chaining)
 import './nonterminal/from-xml';
 import './nonterminal/from-json';
 import './nonterminal/with-config';
 import './nonterminal/with-transforms';
-import './nonterminal/config-management';
 
 // No exports - this file is only imported for its side effects
